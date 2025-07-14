b4fc0d9de6b7c25260bdbc52f10b53a7ed9844c7
xinxiongdi <97471993+tsxing@users.noreply.github.com>
2025-07-14 11:37:51 -0400

    Update Main.java

--- july 11 hw/2016_2/Main.java
+++ july 11 hw/2016_2/Main.java
@@ -1,75 +1,67 @@
 import java.io.*;
 import java.util.*;
-import java.lang.Math;
-
 
 class Main {
     static int N;
-    static int bales[];
-    public static void main(String[] args) throws IOException {
-	    
-	BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-	PrintWriter pw = new PrintWriter(System.out);
+    static int[] bales;
 
-        
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt(); 
+    public static void main(String[] args) throws IOException {
+        BufferedReader r = new BufferedReader(new FileReader("angry.in"));
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("angry.out")));
+
+        N = Integer.parseInt(r.readLine());
         bales = new int[N];
         for (int i = 0; i < N; i++) {
-            bales[i] = sc.nextInt();
+            bales[i] = Integer.parseInt(r.readLine());
         }
         Arrays.sort(bales);
+
         int ans = 1;
-        
-        for (int i=0;i<N;i++){
+        for (int i = 0; i < N; i++) {
             int r_amt = right_most(i);
             int l_amt = left_most(i);
-            int exploded = r_amt- l_amt+1;
-            if (exploded >ans){
+            int exploded = r_amt - l_amt + 1;
+            if (exploded > ans) {
                 ans = exploded;
             }
         }
+
         pw.println(ans);
         pw.close();
-        br.close();
-        
+        r.close();
     }
-    
-    public static int right_most(int start){
+
+    public static int right_most(int start) {
         int last = start;
         int rad = 1;
-        while (true){
+        while (true) {
             int neww = last;
-            while ((0 <= neww + 1 && neww + 1 < N) && (Math.abs(bales[neww + 1] - bales[last]) <= rad)){
+            while ((0 <= neww + 1 && neww + 1 < N) && (Math.abs(bales[neww + 1] - bales[last]) <= rad)) {
                 neww++;
-                
             }
-            if (neww==last){
+            if (neww == last) {
                 break;
             }
-            last= neww;
+            last = neww;
             rad++;
         }
         return last;
     }
-    
-    public static int left_most(int start){
+
+    public static int left_most(int start) {
         int last = start;
         int rad = 1;
-        while (true){
+        while (true) {
             int neww = last;
-            while ((0 <= neww - 1 && neww - 1 < N) && (Math.abs(bales[neww - 1] - bales[last]) <= rad)){
+            while ((0 <= neww - 1 && neww - 1 < N) && (Math.abs(bales[neww - 1] - bales[last]) <= rad)) {
                 neww--;
-                
             }
-            if (neww==last){
+            if (neww == last) {
                 break;
             }
-            last= neww;
+            last = neww;
             rad++;
         }
         return last;
     }
-    
-    
 }
