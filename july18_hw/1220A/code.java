ebb7ddbc8f669426c5d1fd76f226ffa7e478530c
xinxiongdi <97471993+tsxing@users.noreply.github.com>
2025-07-18 22:28:46 -0400

    Create code.java

---
+++ july18_hw/1220A/code.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        String s=sc.next();
+        char[] chars = s.toCharArray();
+        
+        // make freq list from chars 
+        int cnt0 =0;
+        int cnt1 =0;
+        for (char ch : chars){
+            if (ch== 'z'){
+                cnt0++;
+            }
+            if (ch== 'n'){
+                cnt1++;
+            }
+        }
+        for (int i=0;i<cnt1;i++){
+            System.out.println(1+" ");
+        }
+        for (int i=0;i<cnt0;i++){
+            System.out.println(0+" ");
+        }
+    }
+}
