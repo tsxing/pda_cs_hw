18dede9f177b4f12f04ee0ed1bb387b686efedf5
xinxiongdi <97471993+tsxing@users.noreply.github.com>
2025-07-26 10:40:22 -0400

    Update code.py

--- july 24 hw/Cannonball/code.py
+++ july 24 hw/Cannonball/code.py
@@ -1,1 +1,26 @@
 
+N, S = map(int, input().split())
+log = []
+for _ in range(N):
+    a, b= map(int, input().split())
+    log.append([a,b])
+
+line = [False]*N
+curr_pos = S-1
+curr_dir = 1
+curr_pow = 1
+next_pos = curr_pos + curr_dir *curr_pow
+
+while (0<=next_pos<=N and 0<=curr_pos<=N):
+    if log[curr_pos][0]==0: # jumpad
+
+        curr_pow += log[curr_pos][1]
+        curr_dir *= -1;
+    elif log[curr_pos][0]==1: #target
+     
+        if log[curr_pos][1]<=curr_pow and line[curr_pos] == False: #  break it
+            line[curr_pos] = True
+    curr_pos = curr_pos + curr_dir *curr_pow    
+
+
+print(line.count(True))
