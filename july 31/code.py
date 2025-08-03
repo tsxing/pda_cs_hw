8d87dd071a6a293977401928244e36d400302660
xinxiongdi <97471993+tsxing@users.noreply.github.com>
2025-08-02 21:22:42 -0400

    Create code.py

---
+++ july 31/code.py
@@ -0,0 +1,20 @@
+import sys
+sys.stdin = open("cbarn.in", "r")
+sys.stdout = open("cbarn.out", "w")
+
+n = int(input())
+log=[]
+for _ in range(n):
+    r = int(input())
+    log.append(r)
+    
+ans = 1000000
+for start_door in range(n):
+    dist = 0;
+    tot = sum(log) # total cows
+    remaining = tot #cows remaining
+    for r in range(n):
+        remaining-= log[(start_door+r)%n]
+        dist += remaining
+    ans = min(dist,ans)
+print(ans)
