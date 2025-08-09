808e0fef51e91a0825dd15319ffd0fff10d50043
xinxiongdi <97471993+tsxing@users.noreply.github.com>
2025-08-09 12:15:51 -0400

    Update code.py

--- Aug 7/1676D/code.py
+++ Aug 7/1676D/code.py
@@ -1,1 +1,26 @@
 
+t = int(input())
+
+for _ in range(t):
+    board = []
+    n, m = map(int, input().split())
+    for _ in range(n):
+        line = [int(x) for x in input().split()]
+        board.append(line)
+
+    main_diag = [0] * (n+m-1)  
+    anti_diag = [0] * (n+m-1) 
+
+    for i in range(n):
+        for j in range(m):
+            main_diag[i-j + m-1] += board[i][j]
+            anti_diag[i+j] += board[i][j]
+
+    best_sum = 0
+    for i in range(n):
+        for j in range(m):
+            total = main_diag[i-j + m-1] + anti_diag[i + j] - board[i][j]
+            if total > best_sum:
+                best_sum = total
+
+    print(best_sum)
