c839a3084f59bcf349141874b529535d53f0c7ac
xinxiongdi <97471993+tsxing@users.noreply.github.com>
2025-08-19 21:24:30 -0400

    Update code.java

--- aug 17/1514C/code.java
+++ aug 17/1514C/code.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.math.*;
-
+//https://codeforces.com/problemset/problem/1514/C
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
