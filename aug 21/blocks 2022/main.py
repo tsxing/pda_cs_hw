26c29869fb31a4e5a3b47d100a0db70b23e6dca5
xinxiongdi <97471993+tsxing@users.noreply.github.com>
2025-08-23 13:44:45 -0400

    Create main.py

---
+++ aug 21/blocks 2022/main.py
@@ -0,0 +1,36 @@
+from itertools import permutations
+
+N = int(input())
+blocks=[]
+for _ in range(4):
+    b = list(str(input()))
+    blocks.append(b)
+
+
+
+
+def solve(word,order):
+    for i in range(len(order)):
+        if word[i] in blocks[order[i]]:
+
+            continue
+        else:
+            return False
+    return True
+
+for _ in range(N):
+    word = str(input())
+    positions = list(permutations(range(4), len(word)))
+
+    ans = False
+    for pos in positions:
+
+        if solve(word,pos):
+            ans = True
+            print("YES")
+            break
+    if ans== False:
+        print("NO")
+
+    
+    
