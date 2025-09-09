0ed2b1ff71f1b763e2fb3e9b6407efd953f1f2b3
xinxiongdi <97471993+tsxing@users.noreply.github.com>
2025-09-09 19:03:27 -0400

    Update code.py

--- sept 4/usaco ones/2022 #1/code.py
+++ sept 4/usaco ones/2022 #1/code.py
@@ -1,1 +1,15 @@
 
+n = int(input())
+c = list(map(int, input().split()))
+
+c.sort(reverse=True)
+best_rev = -1
+best_p = 0
+
+for i, p in enumerate(c):
+    rev = p*(i + 1)
+    if rev > best_rev or (rev == best_rev and p < best_p):
+        best_rev= rev
+        best_p= p
+
+print(best_rev, best_p)
