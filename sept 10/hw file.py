9b470bf1d7460f219c396d2fd6b86ac3e10085c6
xinxiongdi <97471993+tsxing@users.noreply.github.com>
2025-09-17 11:25:38 -0400

    Create hw file.py

---
+++ sept 10/hw file.py
@@ -0,0 +1,47 @@
+
+#https://codeforces.com/problemset/problem/1419/A
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input().strip()
+    if n % 2 == 1:  # odd 
+   
+        found = any(int(s[i]) % 2 == 1 for i in range(0, n, 2))
+        print(1 if found else 2)
+        
+    else:  # even?
+        found = any(int(s[i]) % 2 == 0 for i in range(1, n, 2)) 
+        print(2 if found else 1)
+
+
+#https://codeforces.com/contest/1382/problem/B
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    k = 0
+    while k < n and a[k] == 1:
+        k += 1
+    if ((k == n) ^ (k % 2)):
+        print("Second")
+    else:
+        print("First")
+
+
+#https://codeforces.com/problemset/problem/1559/C
+didn't understand
+
+#this was a problem we did in class I think
+#https://codeforces.com/problemset/problem/1826/A 
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    ans =-1
+    for x in range(n+1):
+        cnt = sum(1 for v in a if x >= v)
+        if cnt ==n - x:
+            ans =x
+            break
+    print(ans)
